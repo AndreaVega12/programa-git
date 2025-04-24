9c64bccfee5331bf41694bc168aecb95facc941f
Andrea Vega <andreita29059@hotmail.com>
2025-04-24 17:38:06 -0500

    se agrego dirrecion de email y nombre

---
+++ sesion.php
@@ -0,0 +1,14 @@
+<?php
+$data = [
+    "online"=>true,
+    "menu"=>[
+        "Proveedores",
+        "Productos",
+        "Clientes",
+        "Compras",
+        "Inventario",
+        "Ventas"
+    ]
+];
+
+echo json_encode($data);
